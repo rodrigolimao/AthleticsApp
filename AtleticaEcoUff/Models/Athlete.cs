b821304f708b21578f0a0fb9d91d4342e3be45ab
Rodrigo Lima Camargo Lima <200380021@student.georgianc.on.ca>
2018-10-11 16:09:24 -0400

    changing columns names and navbar

--- AtleticaEcoUff/Models/Athlete.cs
+++ AtleticaEcoUff/Models/Athlete.cs
@@ -9,14 +9,18 @@ namespace AtleticaEcoUff.Models
     public partial class Athlete
     {
         [Key]
+        [Display(Name ="Athletes ID")]
         public int athlete_id { get; set; }
 
         [Required]
         [StringLength(50)]
+        [Display(Name = "Athletes Name")]
         public string athlete_name { get; set; }
 
+        [Display(Name = "Athletes Age")]
         public int athlete_age { get; set; }
 
+        [Display(Name = "Sport ID")]
         public int sport_id { get; set; }
 
         public virtual Sport Sport { get; set; }
--- AtleticaEcoUff/Models/Sport.cs
+++ AtleticaEcoUff/Models/Sport.cs
@@ -15,14 +15,18 @@ namespace AtleticaEcoUff.Models
         }
 
         [Key]
+        [Display(Name = "Sport ID")]
         public int sport_id { get; set; }
 
         [Required]
         [StringLength(25)]
+        [Display(Name = "Sport Name")]
         public string sport_name { get; set; }
 
+        [Display(Name = "Sport Rating")]
         public int sport_rating { get; set; }
 
+        [Display(Name = "Sport Awards")]
         public int sport_awards { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
